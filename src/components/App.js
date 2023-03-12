8415d87a445ea31ff348e1b6e68e437b9b9aaff2
SYED RIYAZ <syedriyaz@192.168.0.101>
2023-03-12 13:55:54 +0530

    add

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,16 @@ const App = () => {
    const emailRef=useRef();
   const passwordRef=useRef();
   const [formInput,setFormInput]=useState({
+    email:"",
+    password:"",
+  });
+  const [displayMsg,setDisplayMsg]=useState({
     emailMsg:"",
     passwordMMsg:"",
   });
   const handleInputChange=(e)=>{
     e.persist();
+    //console.log(e.target,e.target.id,e.target.value);
     if(e.target.id==="inputEmail"){
       setFormInput((state)=>{
         return{
@@ -29,16 +34,41 @@ const App = () => {
   const handleSubmit=(e)=>{
     e.preventDefault();
     if(formInput.email===""){
+      emailRef.current.focus();
+      setDisplayMsg({
+        emailMsg:"",
+        passwordMMsg:"",
+      });
+    }
+    else if(formInput.password===""){
+      passwordRef.current.focus();
+      setDisplayMsg({
+        emailMsg:"",
+      passwordMMsg:"",
+          });
+
+    }else{
+      setDisplayMsg({
+        emailMsg:formInput.email,
+        passwordMMsg:formInput.password,
+      });
+      setFormInput({
+        email:"",
+        password:"",
+      });
+
+    }
+  };
 
   return (
     <div id="main">
       Email
-      <input id="inputEmail" type="text" value={} ref={} onChange={}/><br/>
+      <input id="inputEmail" type="text" value={formInput.email} ref={emailRef} onChange={handleInputChange}/><br/>
       Password
-      <input id="inputPassword" type="text" value={} ref={} onChange={}/><br/>
-      <button id="submitButton" onClick={}>Submit</button><br/>
-      <p id="emailText">Your Email : {}</p>
-      <p id ="passwordText">Your Password : {}</p>
+      <input id="inputPassword" type="text" value={formInput.password} ref={passwordRef} onChange={handleInputChange}/><br/>
+      <button id="submitButton" onClick={handleSubmit}>Submit</button><br/>
+      <p id="emailText">Your Email : {displayMsg.emailMsg}</p>
+      <p id ="passwordText">Your Password : {displayMsg.passwordMMsg}</p>
       
     </div>
   )
